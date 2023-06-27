c3551dc3f2599c817f788cf4311b90d8bfe2c162
Thomas Faria <57811152+ThomasFaria@users.noreply.github.com>
2023-06-27 14:41:27 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-PROJECT_DIR=~/work/ESA-Nowcasting-2024
-git clone https://$GITHUB_TOKEN@github.com/InseeFrLab/ESA-Nowcasting-2023.git $PROJECT_DIR
-chown -R onyxia:users $PROJECT_DIR/
-cd $PROJECT_DIR
-
-git config --global credential.helper store
-
 mc cp -r s3/projet-esa-nowcasting/targets-data/ .
